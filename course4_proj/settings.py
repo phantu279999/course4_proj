62e72b32a3e6697ed343a8804595c1bb33d80c7e
phantu279999 <phantu279999@gmail.com>
2025-01-26 06:41:30 +0700

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -43,7 +43,8 @@ class Dev(Configuration):
         'movies',
         'gh',
 
-        'django_celery_results'
+        'django_celery_results',
+        'django_celery_beat'
     ]
 
     MIDDLEWARE = [
